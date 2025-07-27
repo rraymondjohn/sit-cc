852b07f7af06473e68e57d5bbd62468122fafc00
Raymond Rivera <rraymondjohn@gmail.com>
2025-07-28 07:10:17 +0800

    updated dbUtils

--- backend/utils/dbUtils.js
+++ backend/utils/dbUtils.js
@@ -1,29 +1,36 @@
 import mysql from "mysql2/promise";
 import dbConfig from "../config/dbConfig.js";
 
-const pool = mysql.createPool({
+const connection = await mysql.createConnection({
   host: dbConfig.RDS_ENDPOINT,
-  port: dbConfig.RDS_PORT,
   user: dbConfig.RDS_USER,
   password: dbConfig.RDS_PASSWORD,
   database: dbConfig.RDS_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: dbConfig.RDS_PORT,
 });
 
 export async function getConnection() {
-  const connection = await pool.getConnection();
-  return connection;
+  if (connection.state === "disconnected") {
+    await connection.connect();
+  }
+  if (connection.state === "connected") {
+    return connection;
+  }
+  throw new Error("Database connection is not established");
 }
 
 export async function query(sql, params) {
-  const [rows] = await pool.execute(sql, params);
-  return rows;
+  const connection = await getConnection();
+  try {
+    const [rows] = await connection.query(sql, params);
+    return rows;
+  } finally {
+    connection.release();
+  }
 }
 
 export async function closeConnection() {
-  await pool.end();
+  await getConnection().end();
 }
 
 export default {
